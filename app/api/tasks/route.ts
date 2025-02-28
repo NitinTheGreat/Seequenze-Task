40ddec8bd63f06aec19db3bf49a99d5265ab3829
NitinTheGreat <nitinpandey1304@gmail.com>
2025-02-28 23:18:33 +0530

    fix: build fix

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { connectToDatabase } from "@/lib/mongoose"
-import TaskModel from "@/lib/models/task"
+import TaskModel from "../../../lib/task"
 
 export async function GET(request: NextRequest) {
   try {
